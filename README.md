5ac907c553faa8f088c244216865624c582c3d51
snowron <35639836+snowron@users.noreply.github.com>
2026-01-11 00:47:52 +0300

    bump 1.11 with cursor and trademark things

---
+++ README.md
@@ -0,0 +1,35 @@
+# Included Shortcut for GitLab™
+
+A browser extension that helps you open included YAML files in GitLab™ with one click.
+
+## Features
+
+- Automatically detects `include:` statements in GitLab™ CI/CD YAML files
+- Adds clickable links to quickly navigate to included files
+- Supports all include types:
+  - `local` includes
+  - `remote` includes
+  - `template` includes
+  - `project` includes (with multiple files)
+  - `component` includes
+
+## Installation
+
+1. Clone or download this repository
+2. Open Chrome and navigate to `chrome://extensions/`
+3. Enable "Developer mode"
+4. Click "Load unpacked" and select this folder
+
+## Usage
+
+Simply navigate to any `.yml` or `.yaml` file in your GitLab™ instance. The extension will automatically add clickable icons next to `include:` statements.
+
+![Examples](examples.png)
+
+## Trademark Notice
+
+This extension is not affiliated, endorsed, sponsored, or approved with or by GitLab Inc.
+
+GITLAB is a trademark of GitLab Inc. in the United States and other countries and regions.
+
+For more information about GitLab trademark guidelines, visit: https://handbook.gitlab.com/handbook/marketing/brand-and-product-marketing/brand/brand-activation/trademark-guidelines/
--- background.js
+++ background.js
@@ -4,213 +4,228 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   const parsedYaml = self.jsyaml.load(message, { json: true });
   sendResponse(parsedYaml);
 });
+const gitlabUrlFilter = {
+  url: [{ hostContains: "gitlab" }],
+};
+
+async function injectAnalyzer(tabId) {
+  chrome.scripting.executeScript({
+    target: { tabId },
+    func: analyzeIncludesOnPage,
+  });
+}
+
+// Full page load
 chrome.webNavigation.onCompleted.addListener(
-  async function (e) {
-    chrome.scripting.executeScript({
-      target: {
-        tabId: e.tabId,
-      },
-      func: analyzeIncludesOnPage,
-    });
-  },
-  {
-    url: [
-      {
-        hostContains: "gitlab",
-      },
-    ],
-  }
+  (e) => injectAnalyzer(e.tabId),
+  gitlabUrlFilter
+);
+
+// SPA navigation (History API - pushState/replaceState)
+chrome.webNavigation.onHistoryStateUpdated.addListener(
+  (e) => injectAnalyzer(e.tabId),
+  gitlabUrlFilter
 );
 
 function analyzeIncludesOnPage() {
-  function addButtonToElement(index, url) {
-    const aEl = document.createElement("a");
-    aEl.setAttribute("target", "_blank");
-    aEl.setAttribute("href", url);
-    aEl.classList = "gl-shadow-none! file-line-num";
+  const INCLUDE_TYPES = {
+    LOCAL: "local",
+    REMOTE: "remote",
+    TEMPLATE: "template",
+    PROJECT: "project",
+    COMPONENT: "component",
+  };
 
-    const aElForLineId = document.createElement("a");
-    aElForLineId.setAttribute("target", "_blank");
-    aElForLineId.setAttribute("href", `#L${index}`);
-    aElForLineId.innerText = index;
-    aElForLineId.classList = "file-line-num diff-line-num";
+  const INCLUDE_PATTERNS = {
+    [INCLUDE_TYPES.LOCAL]: / - local/,
+    [INCLUDE_TYPES.REMOTE]: / - remote/,
+    [INCLUDE_TYPES.TEMPLATE]: / - template/,
+    [INCLUDE_TYPES.PROJECT]: / - project/,
+    [INCLUDE_TYPES.COMPONENT]: / - component/,
+  };
 
-    let imageEl = document.createElement("img");
-    imageEl.setAttribute(
-      "src",
-      chrome.runtime.getURL("/images/green-play-icon.png")
-    );
-
-    imageEl.setAttribute("height", "16");
-    imageEl.setAttribute("width", "16");
-    imageEl.setAttribute("alt", "link to included");
-
-    aEl.appendChild(imageEl);
-
-    const divEl = document.createElement("div");
-    divEl.style = "display: flex; flex-grow: 1";
-    divEl.appendChild(aEl);
-    divEl.appendChild(aElForLineId);
-
-    document.getElementById(`L${index}`).replaceWith(divEl);
+  function stripQuotes(str) {
+    if (!str) return str;
+    const firstChar = str[0];
+    if (firstChar === "'" || firstChar === '"') {
+      return str.slice(1, -1);
+    }
+    return str;
   }
 
-  function addSlashToURL(url) {
-    if (url[0] !== "/") {
-      return "/" + url;
-    }
-    return url;
+  function normalizePath(path) {
+    // Remove leading slashes and ensure exactly one
+    return "/" + path.replace(/^\/+/, "");
   }
 
-  function removeTick(url) {
-    if (url[0] === "'") {
-      return url.slice(1, -1);
+  function getProjectBaseUrl() {
+    // URL: https://gitlab.com/group/subgroup/project/-/blob/master/path/file.yml
+    // Returns: https://gitlab.com/group/subgroup/project/-/blob/master
+    const parts = window.location.href.split("/");
+    const blobIndex = parts.indexOf("blob");
+    if (blobIndex !== -1) {
+      // Include up to ref (branch/tag) - blob index + 2 (blob + ref)
+      return parts.slice(0, blobIndex + 2).join("/");
     }
-    return url;
+    // Fallback for tree or other views
+    const treeIndex = parts.indexOf("tree");
+    if (treeIndex !== -1) {
+      return parts.slice(0, treeIndex).join("/") + "/-/blob/" + parts[treeIndex + 1];
+    }
+    return parts.slice(0, 8).join("/");
   }
 
-  function removeDoubleTick(url) {
-    if (url[0] === '"') {
-      return url.slice(1, -1);
-    }
-    return url;
+  function addButtonToElement(lineNumber, url) {
+    const linkEl = document.createElement("a");
+    linkEl.target = "_blank";
+    linkEl.href = url;
+    linkEl.className = "gl-shadow-none! file-line-num";
+
+    const lineAnchorEl = document.createElement("a");
+    lineAnchorEl.target = "_blank";
+    lineAnchorEl.href = `#L${lineNumber}`;
+    lineAnchorEl.innerText = lineNumber;
+    lineAnchorEl.className = "file-line-num diff-line-num";
+
+    const iconEl = document.createElement("img");
+    iconEl.src = chrome.runtime.getURL("/images/green-play-icon.png");
+    iconEl.height = 16;
+    iconEl.width = 16;
+    iconEl.alt = "link to included";
+
+    linkEl.appendChild(iconEl);
+
+    const containerEl = document.createElement("div");
+    containerEl.style.cssText = "display: flex; flex-grow: 1";
+    containerEl.appendChild(linkEl);
+    containerEl.appendChild(lineAnchorEl);
+
+    document.getElementById(`L${lineNumber}`).replaceWith(containerEl);
   }
 
-  function createIncludedLink(type, url) {
-    if (url.includes("http://") || url.includes("https://")) {
-      return url;
+  function buildIncludeUrl(type, path) {
+    const cleanPath = stripQuotes(path);
+
+    if (cleanPath.startsWith("http://") || cleanPath.startsWith("https://")) {
+      return cleanPath;
     }
 
-    url = removeTick(url);
-    url = removeDoubleTick(url);
-
-    const hostWithProjectName = window.location.href
-      .split("/")
-      .slice(0, 8)
-      .join("/");
-    const urlWithSlash = addSlashToURL(url);
-
-    switch (type) {
-      case "local":
-        return `${hostWithProjectName}${urlWithSlash}`;
-      case "remote":
-        return url;
-      case "template":
-        return `${hostWithProjectName}${urlWithSlash}`;
+    if (type === INCLUDE_TYPES.REMOTE) {
+      return cleanPath;
     }
+
+    if (type === INCLUDE_TYPES.TEMPLATE) {
+      return `https://gitlab.com/gitlab-org/gitlab/-/blob/master/lib/gitlab/ci/templates/${cleanPath}`;
+    }
+
+    if (type === INCLUDE_TYPES.COMPONENT) {
+      const [componentPath, version] = cleanPath.split("@");
+      const pathParts = componentPath.split("/");
+      const host = pathParts[0];
+      const componentName = pathParts[pathParts.length - 1];
+      const projectPath = pathParts.slice(1, -1).join("/");
+      const ref = version || "main";
+      return `https://${host}/${projectPath}/-/blob/${ref}/templates/${componentName}.yml`;
+    }
+
+    return `${getProjectBaseUrl()}${normalizePath(cleanPath)}`;
   }
 
-  function createProjectIncludedLink(project, file, ref) {
-    if (project) {
-      project = removeTick(project);
-      project = removeDoubleTick(project);
-      project = addSlashToURL(project);
+  function buildProjectIncludeUrl(project, file, ref) {
+    const cleanProject = normalizePath(stripQuotes(project));
+    const cleanFile = normalizePath(stripQuotes(file));
+    const cleanRef = stripQuotes(ref) || "master";
+
+    return `${window.location.origin}${cleanProject}/-/blob/${cleanRef}${cleanFile}`;
+  }
+
+  function detectIncludeType(lineText) {
+    for (const [type, pattern] of Object.entries(INCLUDE_PATTERNS)) {
+      if (pattern.test(lineText)) {
+        return type;
+      }
+    }
+    return null;
+  }
+
+  function collectYamlContent() {
+    let lineNumber = 1;
+    let yamlContent = "";
+    const linesByType = {
+      [INCLUDE_TYPES.LOCAL]: [],
+      [INCLUDE_TYPES.REMOTE]: [],
+      [INCLUDE_TYPES.TEMPLATE]: [],
+      [INCLUDE_TYPES.PROJECT]: [],
+      [INCLUDE_TYPES.COMPONENT]: [],
+      simpleString: [],
+    };
+
+    while (document.getElementById(`LC${lineNumber}`) !== null) {
+      const lineEl = document.getElementById(`LC${lineNumber}`);
+      const lineText = lineEl.innerText;
+
+      yamlContent += "\n" + lineText;
+
+      const includeType = detectIncludeType(lineText);
+      if (includeType) {
+        linesByType[includeType].push(lineNumber);
+      } else if (!lineText.includes("include:")) {
+        linesByType.simpleString.push(lineNumber);
+      }
+
+      lineNumber++;
     }
 
-    if (file) {
-      file = removeTick(file);
-      file = removeDoubleTick(file);
-      file = addSlashToURL(file);
+    return { yamlContent, linesByType };
+  }
+
+  function processIncludeItem(include, linesByType) {
+    if (typeof include === "string") {
+      const lineNumber = linesByType.simpleString.shift();
+      if (lineNumber) {
+        addButtonToElement(lineNumber, buildIncludeUrl(INCLUDE_TYPES.LOCAL, include));
+      }
+      return;
     }
 
-    if (ref) {
-      ref = removeTick(ref);
-      ref = removeDoubleTick(ref);
+    if (typeof include !== "object") return;
+
+    const simpleTypes = [INCLUDE_TYPES.LOCAL, INCLUDE_TYPES.TEMPLATE, INCLUDE_TYPES.REMOTE, INCLUDE_TYPES.COMPONENT];
+    
+    for (const type of simpleTypes) {
+      if (include[type]) {
+        const lineNumber = linesByType[type].shift();
+        if (lineNumber) {
+          addButtonToElement(lineNumber, buildIncludeUrl(type, include[type]));
+        }
+        return;
+      }
     }
 
-    const host = window.location.origin;
+    if (include[INCLUDE_TYPES.PROJECT]) {
+      const files = Array.isArray(include.file) ? include.file : [include.file];
+      const baseLineNumber = linesByType[INCLUDE_TYPES.PROJECT].shift();
 
-    return `${host}${project}/-/blob/${ref || "master"}${file}`;
+      files.forEach((file, index) => {
+        const lineOffset = files.length > 1 ? index + 3 : 0;
+        addButtonToElement(
+          baseLineNumber + lineOffset,
+          buildProjectIncludeUrl(include.project, file, include.ref)
+        );
+      });
+    }
   }
 
   async function process() {
-    let i = 1;
-    let yamlContent = "";
-    const arrayOfProperties = {
-      local: [],
-      remote: [],
-      template: [],
-      project: [],
-      simpleString: [],
-    };
-    while (document.getElementById(`LC${i}`) !== null) {
-      const line = document.getElementById(`LC${i}`);
-      try {
-        yamlContent += "\n" + line.innerText;
-        if (line.innerText.includes(" - local")) {
-          arrayOfProperties["local"].push(i);
-        } else if (line.innerText.includes(" - remote")) {
-          arrayOfProperties["remote"].push(i);
-        } else if (line.innerText.includes(" - template")) {
-          arrayOfProperties["template"].push(i);
-        } else if (line.innerText.includes(" - project")) {
-          arrayOfProperties["project"].push(i);
-        } else if (!line.innerText.includes("include:")) {
-          arrayOfProperties.simpleString.push(i);
-        }
-      } catch (error) {}
-
-      i++;
-    }
-
+    const { yamlContent, linesByType } = collectYamlContent();
     const parsedYaml = await chrome.runtime.sendMessage(yamlContent);
 
-    if (parsedYaml.include.length > 0) {
-      for (const property of parsedYaml.include) {
-        try {
-          if (typeof property === "string") {
-            addButtonToElement(
-              arrayOfProperties.simpleString[0],
-              createIncludedLink("local", property)
-            );
-            arrayOfProperties.simpleString.shift();
-          } else if (typeof property === "object") {
-            if (property["local"]) {
-              addButtonToElement(
-                arrayOfProperties.local[0],
-                createIncludedLink("template", property["local"])
-              );
-              arrayOfProperties.local.shift();
-            } else if (property["template"]) {
-              addButtonToElement(
-                arrayOfProperties.template[0],
+    if (!parsedYaml?.include?.length) return;
 
-                createIncludedLink("template", property["template"])
-              );
-              arrayOfProperties.template.shift();
-            } else if (property["remote"]) {
-              addButtonToElement(
-                arrayOfProperties.remote[0],
-                createIncludedLink("remote", property["remote"])
-              );
-              arrayOfProperties.remote.shift();
-            } else if (property["project"]) {
-              if (Array.isArray(property["file"])) {
-                for (let index = 0; index < property["file"].length; index++) {
-                  addButtonToElement(
-                    arrayOfProperties.project[0] + index + 3,
-                    createProjectIncludedLink(
-                      property["project"],
-                      property["file"][index],
-                      property["ref"]
-                    )
-                  );
-                }
-              } else {
-                addButtonToElement(
-                  arrayOfProperties.project[0],
-                  createProjectIncludedLink(
-                    property["project"],
-                    property["file"],
-                    property["ref"]
-                  )
-                );
-              }
-              arrayOfProperties.project.shift();
-            }
-          }
-        } catch (error) {}
-      }
+    for (const include of parsedYaml.include) {
+      try {
+        processIncludeItem(include, linesByType);
+      } catch (error) {}
     }
   }
 
